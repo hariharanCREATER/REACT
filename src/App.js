0982e37962775f0c868c576b9aacd63421af4df6
hariharanCREATER <161237612+hariharanCREATER@users.noreply.github.com>
2024-10-09 18:29:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>REACT LEARN</h1>
+      <p>Reac is Framework Which Have a Virtual DOM</p>
+     
+      
+    </div>
+
+  
+  );
+}
+
+export default App;
